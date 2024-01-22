4839b4fc48fb3a8bdb6339e3049c58b127dfa01a
Keamogetswe Kgakatsi <stimulatekay11ew@gmail.com>
2024-01-22 09:00:11 +0200

    logo display

--- components/Navabar.jsx
+++ components/Navabar.jsx
@@ -1,17 +1,23 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 import React from 'react'
 
-const Navabar = () => {
+const Navbar = () => {
   return (
+    // Navbar container
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
-        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-        <Image 
-          src="/../public/assets/projects/nextjs.png" 
-          alt="/" 
-          width="125" 
-          height="50" 
+      {/* Navbar content */}
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        {/* Logo */}
+        <Image
+          src="/assets/projects/nextjs.png"
+          alt="/"
+          width="125"
+          height="50"
         />
+
+        {/* Desktop navigation links */}
         <ul className='hidden md:flex'>
           <Link href="/">
             <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
@@ -30,19 +36,24 @@ const Navabar = () => {
           </Link>
         </ul>
 
-        /** Mobile view*/
+        {/* Mobile view - Menu icon */}
         <div className='md:hidden'>
-          /** React icons here */
+          <AiOutlineMenu size={25} />
         </div>
-        
-        </div>
+      </div>
 
-        /** overlay */
-        <div className='fixed left-0 top-0 w-full h-screen bg-black/75'>
-
+      {/* Mobile view - Overlay */}
+      <div className='fixed left-0 top-0 w-full h-screen bg-black/75'>
+      <div className='fixed left-0 top-0 w=[75%] sm:w=[60%] md:w=[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+        <div>
+          <div>
+            <Image src="/assets/navBarLogo.png"  width='87' height='35' alt="/" />
+          </div>
         </div>
+      </div>
+      </div>
     </div>
   )
 }
 
-export default Navabar
+export default Navbar
