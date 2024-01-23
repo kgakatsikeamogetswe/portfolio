1a38729db33758d1f259a6375b1a958c57d28a6b
Keamogetswe Kgakatsi <stimulatekay11ew@gmail.com>
2024-01-24 01:45:32 +0200

    navbar layout

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from 'react-icons/ai'
+import {FaLinkedinIn, FaGithub} from 'react-icons/fa'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import React from 'react'
 
 const Navbar = () => {
@@ -50,6 +52,46 @@ const Navbar = () => {
             <Image src="/assets/navBarLogo.png"  width='87' height='35' alt="/" />
             <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
               <AiOutlineClose />
+            </div>
+          </div>
+          <div className='border-b border-gray-500 my-4'>
+            <p className='w=[85%] md:w=[90%] my-4'>Welcome to my portfolio.</p>
+          </div>
+        </div>
+        <div className='py-4 flex flex-col'>
+          <ul className='uppercase'>
+            <Link href='/'>
+            <li className='py-4 text-sm'>Home</li>
+            </Link>
+            <Link href='/'>
+            <li className='py-4 text-sm'>About</li>
+            </Link>
+            <Link href='/'>
+            <li className='py-4 text-sm'>Skills</li>
+            </Link>
+            <Link href='/'>
+            <li className='py-4 text-sm'>Projects</li>
+            </Link>
+            <Link href='/'>
+            <li className='py-4 text-sm'>Contact</li>
+            </Link>
+          </ul>
+
+          <div className='pt-40'>
+            <p className='uppercase tracking-widest text-[#2c1101e3]'>Let's connect</p>
+            <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
+              <FaLinkedinIn/>
+              </div>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
+              <FaGithub/>
+              </div>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
+              <AiOutlineMail/>
+              </div>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
+              <BsFillPersonLinesFill/>
+              </div>
             </div>
           </div>
         </div>
