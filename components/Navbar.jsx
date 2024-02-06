2f231b00c1a16a6544ae47fa3a04e15263f72dea
Keamogetswe Kgakatsi <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2024-02-06 14:40:30 +0200

    fixing ' error

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -86,7 +86,7 @@ const Navbar = () => {
           </ul>
 
           <div className='pt-40'>
-            <p className='uppercase tracking-widest text-[#2c1101e3]'>Let's connect</p>
+            <p className='uppercase tracking-widest text-[#2c1101e3]'>Connect with me</p>
             <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
               <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
               <FaLinkedinIn/>
