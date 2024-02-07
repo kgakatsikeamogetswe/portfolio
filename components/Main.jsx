87b3968a204365caf0ddb931ac538983e44667b1
Keamogetswe Kgakatsi <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2024-02-07 14:30:49 +0200

    added animation

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,7 +5,7 @@ function Main() {
     <div className='w-full h-screen text-center'>
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
-                <p className='uppercase text-sm tracking-widest text-grey-600'>Feel free to browse my portfolio</p>
+                <p className='uppercase text-sm tracking-widest text-grey-600 animate-jump animate-infinite animate-duration-[5000ms] animate-delay-[10ms] animate-ease-in-out animate-alternate-reverse'>Feel free to browse my portfolio</p>
             </div>
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,18 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animationDelay: {
+        275: '275ms',
+        5000: '5s',
+      },
+      animationDuration: {
+        2000: '2s',
+        'long': '10s',
+        'very-long': '20s',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated')
+  ],
 };
