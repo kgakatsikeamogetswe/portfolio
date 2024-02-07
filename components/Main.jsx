25a5ecf540839fcdfe4f1fe0c4c7eed34eb29389
Keamogetswe Kgakatsi <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2024-02-07 17:22:20 +0200

    clearing

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,12 +8,12 @@ function Main() {
     <div className='w-full h-screen flex justify-center items-center'>
       <div className='max-w-[1240px] w-full p-2'>
         
-        <div className='h-16'></div>
+        <div className='h-20 flex flex-col items-center'></div>
 
         <div className='flex flex-col items-center'>
-          <p className='uppercase text-sm tracking-widest text-gray-600 py-4'>Feel free to browse my portfolio</p>
-          <h1 className='py-4 text-[#5b5b5b]  animate-bounce animate-infinite animate-duration-[5000ms] animate-delay-[10ms] animate-ease-in-out animate-alternate-reverse'>Hi, I'm <span className='text-[#302f2f]'>Keamogetswe</span></h1>
-          <h1 className='py-2 text-[#5b5b5b]'>A Front-End Developer</h1>
+          <p className='uppercase text-sm tracking-widest text-gray-600 py-4 max-w-[70%] text-center'>Feel free to browse my portfolio</p>
+          <h1 className='py-4 text-[#5b5b5b] max-w-[70%] text-center animate-bounce animate-infinite animate-duration-[5000ms] animate-delay-[10ms] animate-ease-in-out animate-alternate-reverse'>Hi, I'm <span className='text-[#302f2f]'>Keamogetswe</span></h1>
+          <h1 className='py-2 text-[#5b5b5b] max-w-[70%] text-center'>A Front-End Developer</h1>
           <p className='py-4 text-gray-600 max-w-[70%] text-center'>
           As a front-end developer with expertise in building responsive web applications using React, Next.js, JavaScript, HTML, and CSS, I am currently honing my backend skills with a focus on MongoDB. I acquired these skills through my education at Codespace, where I graduated and specialized in JavaScript. I am dedicated to furthering my knowledge and proficiency in backend development, particularly in MongoDB, to complement my front-end expertise.
           </p>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,14 +15,15 @@ const Navbar = () => {
   return (
     // Navbar container
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
-      {/* Navbar content */}
+      {/* Navbar content */ }
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
         {/* Logo */}
         <Image
           src="/assets/navBarLogo.png"
           alt="/"
           width="125"
-          height="50"
+          height="10"
+          className='h-20'
         />
 
         {/* Desktop navigation links */}
@@ -51,13 +52,13 @@ const Navbar = () => {
       </div>
 
       {/* Mobile view - Overlay */}
-      <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/75' : '' }>
+      <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/75' : ''}>
       <div className={nav 
                     ? 'fixed left-0 top-0 w=[75%] sm:w=[60%] md:w=[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' 
                     : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
         <div>
           <div className='flex w-full text-center justify-between'>
-            <Image src="/assets/navBarLogo.png"  width='87' height='35' alt="/" />
+            <Image src="/assets/navBarLogo.png"  width='87' height='25' alt="/" />
             <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
               <AiOutlineClose />
             </div>
