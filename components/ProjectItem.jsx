86c746e0c2ba2d86f35157dbe94ebbc3da6ec58d
Keamogetswe Kgakatsi <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2024-02-08 18:59:44 +0200

    clearing error

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image'
 
-const ProjectItem = ({title, backgroudImg, projectUrl}) => {
+const ProjectItem = ({title, backgroundImg, projectUrl}) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#333333] to-[#afafaf]'>
-    <Image className='rounded-xl group-hover:opacity-10' src={backgroudImg} alt='/' />
+    <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' />
     <div className='hidden group-hover:black absolute top-[50%] left-[50%] transale-x-[-50%] translate-y-[50%]'>
       <h3 className='text-2xl text-white tracking-widest text-center'>{title}</h3>
       <p className='pb-4 pt-2 text-white text-center'>JAvascript</p>
