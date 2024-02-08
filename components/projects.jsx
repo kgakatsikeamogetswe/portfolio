36724411dd45583bf4784bcd4f5f0142d28becd2
Keamogetswe Kgakatsi <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2024-02-08 18:52:25 +0200

    props for projectItem

--- components/projects.jsx
+++ components/projects.jsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import React from 'react';
 import Link from 'next/link';
 import mongodbImg from '../public/assets/projects/mongodb.png'
+import nextjsImg from '../public/assets/projects/nextjs,png'
+import ProjectItem from './ProjectItem'
 
 const projects = () => {
   return (
@@ -10,18 +12,26 @@ const projects = () => {
         <p className='text-xl tracking-widest uppercase text-[#5b5b5b]'>Projects</p>
         <h2 className='py-4'>What I've build!</h2>
         <div className='grid md:grid-cols-2 gap-8'>
-
-          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#333333] to-[#afafaf]'>
-            <Image className='rounded-xl group-hover:opacity-10' src={mongodbImg} alt='/' />
-            <div className='hidden group-hover:black absolute top-[50%] left-[50%] transale-x-[-50%] translate-y-[50%]'>
-              <h3 className='text-2xl text-white tracking-widest text-center'>Mongodb project</h3>
-              <p className='pb-4 pt-2 text-white text-center'>JAvascript</p>
-              <Link href='/'>
-              <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
-              </Link>
-
-            </div>
-          </div>
+          <ProjectItem 
+          title='mongodb projects' 
+          backgroundImg={mongodbImg} 
+          projectUrl='/'
+          />
+          <ProjectItem 
+          title='nextjs' 
+          backgroundImg={nextjsImg} 
+          projectUrl='/'
+          />
+          <ProjectItem 
+          title='mongodb projects' 
+          backgroundImg={mongodbImg} 
+          projectUrl='/'
+          />
+          <ProjectItem 
+          title='mongodb projects' 
+          backgroundImg={mongodbImg} 
+          projectUrl='/'
+          />
         </div>
       </div>
     </div>
