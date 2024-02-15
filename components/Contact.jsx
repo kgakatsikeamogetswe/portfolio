2de75a266db8539b037f3bb400a849494f17e6f7
Keamogetswe Kgakatsi <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2024-02-15 18:50:37 +0200

    contact details

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,7 +8,20 @@ const Contact = () => {
             <h2 className='py-4'>Connect with me</h2>
             <div className='grid lg:grid-cols-5 gap-8'></div>
             {/**left */}
-            
+            <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
+                <div className='lg:p-4 h-full'>
+                    <div>
+                        <img className='rounded-xl hover:scale-105 ease-in duration-300' src="" alt="/" />
+                    </div>
+                    <div>
+                        <h2 className='py-2'>Name here</h2>
+                        <p>Front-End Developer</p>
+                        <p>I am available anytime for any role</p>
+                    </div>
+
+                </div>
+            </div>
+
 
             {/**right */}
         </div>
