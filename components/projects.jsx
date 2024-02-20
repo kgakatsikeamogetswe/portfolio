237c3f22eb5bbdfd3ba576606ce5acd4087f042b
Keamogetswe Kgakatsi <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2024-02-20 10:16:09 +0200

    eslinting

--- components/projects.jsx
+++ components/projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
     <div id='projects' className='w-full'>
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
         <p className='text-xl tracking-widest uppercase text-[#5b5b5b]'>Projects</p>
-        <h2 className='py-4'>What I've built!</h2>
+        <h2 className='py-4'>What I have built!</h2>
         <div className='grid md:grid-cols-2 gap-8'>
           <ProjectItem 
             title='Foodies Delight' 
