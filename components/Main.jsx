2226b6dd93c722c7ef917931d0e3770fbd6fd4fb
Keamogetswe Kgakatsi <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2024-02-20 09:48:27 +0200

    fixing title

--- components/Main.jsx
+++ components/Main.jsx
@@ -14,10 +14,10 @@ function Main() {
           <p className='uppercase text-sm tracking-widest text-gray-600 py-4 max-w-[70%] text-center'>Feel free to browse my portfolio</p>
           <h1 className='py-4 text-[#5b5b5b] max-w-[70%] text-center animate-bounce animate-infinite animate-duration-[5000ms] animate-delay-[10ms] animate-ease-in-out animate-alternate-reverse'>
             Hi, I'm 
-            <span className='text-[#302f2f]'>Keamogetswe</span>
+            <span className='text-[#302f2f] ml-3'>Keamogetswe</span>
           </h1>
           <h1 className='py-2 text-[#5b5b5b] max-w-[70%] text-center'>A Front-End Developer</h1>
-          <p className='py-4 text-gray-600 max-w-[70%] text-center'>
+          <p className='py-4 pr-5 text-gray-600 max-w-[70%] text-center'>
           As a front-end developer with expertise in building responsive web applications using React, Next.js, JavaScript, HTML, and CSS, I am currently honing my backend skills with a focus on MongoDB. I acquired these skills through my education at Codespace, where I graduated and specialized in JavaScript. I am dedicated to furthering my knowledge and proficiency in backend development, particularly in MongoDB, to complement my front-end expertise.
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
