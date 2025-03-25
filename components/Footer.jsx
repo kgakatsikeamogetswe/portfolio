ce2714f55bd8bb480437de248c69488f8b08b213
Keamogetswe Kgakatsi <stimulatekay11ew@gmail.com>
2025-03-25 06:48:17 -0700

    footer deleted

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,41 +1,41 @@
-import Link from 'next/link';
-import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
-import { AiOutlineMail } from 'react-icons/ai';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
+// import Link from 'next/link';
+// import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
+// import { AiOutlineMail } from 'react-icons/ai';
+// import { BsFillPersonLinesFill } from 'react-icons/bs';
 
-const Footer = () => {
-  return (
-    <footer className="w-full bg-[#ecf0f3] shadow-xl mt-10 py-6">
-      <div className="flex flex-col items-center justify-center">
-        <p className="text-gray-700 text-sm">&copy; {new Date().getFullYear()} Keamogetswe Kgakatsi. All rights reserved.</p>
-        <div className="flex items-center gap-4 mt-4">
-          <Link href='https://www.linkedin.com/in/keamogetswe-kgakatsi-388239267'>
-            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
-              <FaLinkedinIn />
-            </div>
-          </Link>
+// const Footer = () => {
+//   return (
+//     <footer className="w-full bg-[#ecf0f3] shadow-xl mt-10 py-6">
+//       <div className="flex flex-col items-center justify-center">
+//         <p className="text-gray-700 text-sm">&copy; {new Date().getFullYear()} Keamogetswe Kgakatsi. All rights reserved.</p>
+//         <div className="flex items-center gap-4 mt-4">
+//           <Link href='https://www.linkedin.com/in/keamogetswe-kgakatsi-388239267'>
+//             <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
+//               <FaLinkedinIn />
+//             </div>
+//           </Link>
 
-          <Link href='https://github.com/kgakatsikeamogetswe'>
-            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
-              <FaGithub />
-            </div>
-          </Link>
+//           <Link href='https://github.com/kgakatsikeamogetswe'>
+//             <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
+//               <FaGithub />
+//             </div>
+//           </Link>
 
-          <Link href='mailto:keamogetswekgakatsi11@gmail.com'>
-            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
-              <AiOutlineMail />
-            </div>
-          </Link>
+//           <Link href='mailto:keamogetswekgakatsi11@gmail.com'>
+//             <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
+//               <AiOutlineMail />
+//             </div>
+//           </Link>
 
-          <Link href='tel:+27679777367'>
-            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
-              <BsFillPersonLinesFill />
-            </div>
-          </Link>
-        </div>
-      </div>
-    </footer>
-  );
-};
+//           <Link href='tel:+27679777367'>
+//             <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-100'>
+//               <BsFillPersonLinesFill />
+//             </div>
+//           </Link>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// };
 
-export default Footer;
+// export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ function MyApp({ Component, pageProps }) {
   return  <>
             <Navbar/>
             <Component {...pageProps} />
-            <Footer/>
           </>; 
 }
 
